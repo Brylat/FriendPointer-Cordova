4098f01401ed5298b6cc1dfaa1c0fc91b560017c
Brylat <jankowski.andrzej@outlook.com>
2019-05-25 15:54:07 +0200

    #9 firebase service login/registration, change frame

--- www/js/events.js
+++
@@ -1,8 +0,0 @@
-async function afterEventLoad(data) {
-	loadingElement('events');
-	updateButtonStatus('events', 'friends', 'map');
-    console.log("dane", data);
-
-    $("#customIdEvent").text("dupa");
-	closeLoading('events');
-}
--- www/js/map.js
+++
@@ -1,8 +0,0 @@
-async function afterMapLoad(data) {
-	loadingElement('map');
-	updateButtonStatus('map', 'events', 'friends');
-	console.log("dane", data);
-
-	$("#customIdMap").text("dupa");
-	closeLoading('map');
-}
