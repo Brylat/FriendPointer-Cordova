9d4537c01156d9a75d40c294f56ae1a914636046
ChuZZZta <bodzio223@gmail.com>
2019-05-28 22:05:41 +0200

    #17 Added changing name,surname,destription feature

--- src/pages/settings-screen/settings-screen.page.ts
+++ src/pages/settings-screen/settings-screen.page.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { DatabaseService } from '../../services/database.service';
 
 @Component({
 	templateUrl: 'settings-screen.html'
 })
 export class SettingsScreenPage {
-	constructor() {
+
+	private databaseService: DatabaseService;
+	constructor(databaseService: DatabaseService) {
+		this.databaseService = databaseService;
+		this.getData();
 	}
+	
+	public name: string = "";
+	public surname: string = "";
+	public destription: string = "";
+
+	private async getData()
+	{
+		let user = await this.databaseService.getCurrentUserData();
+		this.name = user.name;
+		this.surname = user.surname;
+		this.destription = user.description;
+		console.log(user);
+	}
+	private async updateDestription()
+	{
+		let user = await this.databaseService.getCurrentUserData();
+		user.description = this.destription;
+		//console.log(user);
+		await this.databaseService.createOrUpdateUser(user);
+	}
+	private async updateNameSurname()
+	{
+		let user = await this.databaseService.getCurrentUserData();
+		user.name = this.name;
+		user.surname = this.surname;
+		//console.log(user);
+		await this.databaseService.createOrUpdateUser(user);
+	}
+	
 
 }
