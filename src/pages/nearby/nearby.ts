d9dd302191d0f281f466fc42d9a3a371eea048ee
Leszek Gędek <leszek.gedek@gmail.com>
2019-06-01 14:25:00 +0200

    #16 Implemented checking for event fullness.

--- src/pages/nearby/nearby.ts
+++ src/pages/nearby/nearby.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {IonicPage, LoadingController, NavController, NavParams} from 'ionic-angular';
+import {AlertController, IonicPage, LoadingController, NavController, NavParams} from 'ionic-angular';
 import {DatabaseService} from "../../services/database.service";
 import {AuthService} from "../../services/auth.service";
 
@@ -12,66 +12,86 @@ import {AuthService} from "../../services/auth.service";
 
 @IonicPage()
 @Component({
-  selector: 'page-nearby',
-  templateUrl: 'nearby.html',
+    selector: 'page-nearby',
+    templateUrl: 'nearby.html',
 })
+
 export class NearbyPage {
 
-  private databaseService: DatabaseService;
-  private authService: AuthService;
-  private eventList: Array<Object>;
-  constructor(
-      private navCtrl: NavController,
-      private navParams: NavParams,
-      databaseService: DatabaseService,
-      authService: AuthService,
-      private loadingCtrl: LoadingController) {
-    this.databaseService = databaseService;
-    this.authService = authService;
-    this.eventList = Array();
-  }
+    private databaseService: DatabaseService;
+    private authService: AuthService;
+    private eventList: Array<Object>;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad NearbyPage');
-    const loader = this.loadingCtrl.create({
-      content: "Please wait..."
-    });
-    loader.present();
-    this.databaseService.getAllEvents().then((res) => {
-      let responseList = (res);
-      for (let response of responseList) {
-        this.eventList.push(
-            {
-              uid: response.uid,
-              name: response.name,
-              ownerUid: response.ownerUid,
-              description: response.description,
-              limit: response.limit,
-              localization: response.localization,
-              createDate: new Date(response.createDate),
-              stopDate: new Date(response.stopDate),
-              participants: response.participants,
+    constructor(
+        private navCtrl: NavController,
+        private navParams: NavParams,
+        databaseService: DatabaseService,
+        authService: AuthService,
+        private alertCtrl: AlertController,
+        private loadingCtrl: LoadingController) {
+        this.databaseService = databaseService;
+        this.authService = authService;
+        this.eventList = Array();
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad NearbyPage');
+        const loader = this.loadingCtrl.create({
+            content: "Please wait..."
+        });
+        loader.present();
+        this.databaseService.getAllEvents().then((res) => {
+            let responseList = (res);
+            for (let response of responseList) {
+                this.eventList.push(
+                    {
+                        uid: response.uid,
+                        name: response.name,
+                        ownerUid: response.ownerUid,
+                        description: response.description,
+                        limit: response.limit,
+                        localization: response.localization,
+                        createDate: new Date(response.createDate),
+                        stopDate: new Date(response.stopDate),
+                        participants: response.participants,
+                    }
+                );
             }
-        );
-      }
-      loader.dismissAll();
-    });
-  }
+            loader.dismissAll();
+        });
+    }
 
 
-  join(referer, eventId) {
-      let uid = this.authService.getUID();
-      let referer_status = referer.currentTarget.style.color;
-      if (referer_status === "") {
-          this.databaseService.addParticipant(eventId, uid).then(
-              (res) => {
-                  referer.currentTarget.style.color = "#4cbb17";
-              });
-      } else {
-          this.databaseService.deleteParticipant(eventId, uid).then(
-              (res) => {
-                  referer.currentTarget.style.color = "";
-              });
-      }
-  }
+    join(referer, event) {
+        let uid = this.authService.getUID();
+        let referer_status = referer.currentTarget.style.color;
+        if (referer_status === "" && event.participants.length !== Number(event.limit)) {
+            this.databaseService.addParticipant(event.uid, uid).then(
+                (res) => {
+                    referer.currentTarget.style.color = "#4cbb17";
+                    event.participants.push(uid);
+                });
+        } else if (referer_status === "" && event.participants.length === Number(event.limit)) {
+            this.eventAlert(event);
+        } else {
+            this.databaseService.deleteParticipant(event.uid, uid).then(
+                (res) => {
+                    event.participants = event.participants.filter(x => x !== uid);
+                    referer.currentTarget.style.color = "";
+                });
+        }
+    }
+
+    goToEvent(event) {
+        alert('My name is: "' + event.name + '" and want to go to my location!');
+    }
+
+    async eventAlert(event) {
+        let alert = this.alertCtrl.create({
+            title: 'Event is full',
+            subTitle: 'Event "' + event.name + '" is full, you cannot join it.',
+            buttons: ['Ok'],
+        });
+        return await alert.present();
+    }
 }
