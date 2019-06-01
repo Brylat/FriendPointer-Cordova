c6bbf6c038da4b8f6c51044c1e81aa78061e30f9
Leszek Gędek <leszek.gedek@gmail.com>
2019-06-01 16:22:41 +0200

    #16 Refactored your-events to final form.

--- src/pages/your/your.ts
+++ src/pages/your/your.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import {IonicPage, LoadingController, NavController, NavParams} from 'ionic-angular';
-import {DatabaseService} from "../../services/database.service";
+import { AlertController, IonicPage, LoadingController, NavController, NavParams } from 'ionic-angular';
+import { DatabaseService } from '../../services/database.service';
 
 /**
  * Generated class for the YourPage page.
@@ -16,41 +16,47 @@ import {DatabaseService} from "../../services/database.service";
 })
 export class YourPage {
 
-  private databaseService: DatabaseService;
-  private eventList: Array<Object>;
-  constructor(
-      private navCtrl: NavController,
-      private navParams: NavParams,
-      databaseService: DatabaseService,
-      private loadingCtrl: LoadingController) {
-    this.databaseService = databaseService;
-    this.eventList = Array();
-  }
+    private databaseService: DatabaseService;
+    private eventList: Array<Object>;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad YourPage');
-    const loader = this.loadingCtrl.create({
-      content: "Please wait..."
-    });
-    loader.present();
-    this.databaseService.getAllOwnEvents().then((res) => {
-      let responseList = (res);
-      for (let response of responseList) {
-        this.eventList.push(
-            {
-              uid: response.uid,
-              name: response.name,
-              ownerUid: response.ownerUid,
-              description: response.description,
-              limit: response.limit,
-              localization: response.localization,
-              createDate: new Date(response.createDate),
-              stopDate: new Date(response.stopDate),
-              participants: response.participants,
+    constructor(
+        private navCtrl: NavController,
+        private navParams: NavParams,
+        databaseService: DatabaseService,
+        private alertCtrl: AlertController,
+        private loadingCtrl: LoadingController) {
+        this.databaseService = databaseService;
+        this.eventList = Array();
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad YourPage');
+        const loader = this.loadingCtrl.create({
+            content: "Please wait..."
+        });
+        loader.present();
+        this.databaseService.getAllEvents().then((res) => {
+            let responseList = (res);
+            for (let response of responseList) {
+                this.eventList.push(
+                    {
+                        uid: response.uid,
+                        name: response.name,
+                        ownerUid: response.ownerUid,
+                        description: response.description,
+                        limit: response.limit,
+                        localization: response.localization,
+                        createDate: new Date(response.createDate),
+                        stopDate: new Date(response.stopDate),
+                        participants: response.participants,
+                    }
+                );
             }
-        );
-      }
-      loader.dismissAll();
-    });
-  }
+            loader.dismissAll();
+        });
+    }
+
+    goToEvent(event) {
+        alert('My name is: "' + event.name + '" and want to go to my location!');
+    }
 }
