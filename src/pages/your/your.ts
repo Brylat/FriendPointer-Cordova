cde5817400dacee737bdf2ef2c879c001e12dc4b
Leszek Gędek <leszek.gedek@gmail.com>
2019-06-01 13:10:32 +0200

    #16 Implemented basic boilerplate for your-events screen.

--- src/pages/your/your.ts
+++ src/pages/your/your.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, LoadingController, NavController, NavParams} from 'ionic-angular';
+import {DatabaseService} from "../../services/database.service";
 
 /**
  * Generated class for the YourPage page.
@@ -15,11 +16,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class YourPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private databaseService: DatabaseService;
+  private eventList: Array<Object>;
+  constructor(
+      private navCtrl: NavController,
+      private navParams: NavParams,
+      databaseService: DatabaseService,
+      private loadingCtrl: LoadingController) {
+    this.databaseService = databaseService;
+    this.eventList = Array();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad YourPage');
+    const loader = this.loadingCtrl.create({
+      content: "Please wait..."
+    });
+    loader.present();
+    this.databaseService.getAllOwnEvents().then((res) => {
+      let responseList = (res);
+      for (let response of responseList) {
+        this.eventList.push(
+            {
+              uid: response.uid,
+              name: response.name,
+              ownerUid: response.ownerUid,
+              description: response.description,
+              limit: response.limit,
+              localization: response.localization,
+              createDate: new Date(response.createDate),
+              stopDate: new Date(response.stopDate),
+              participants: response.participants,
+            }
+        );
+      }
+      loader.dismissAll();
+    });
   }
-
 }
